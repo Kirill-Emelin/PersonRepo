84a4cbe4dfee678b8b7bc232bb30760c2e0836a3
Kirill-Emelin <128322099+Kirill-Emelin@users.noreply.github.com>
2023-07-26 03:07:04 +0200

    Add project files.

---
+++ PersonRepo/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace PersonRepo
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+
+        public Person(string name, string gender)
+        {
+            Name = name;
+            Gender = gender;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}({Gender})";
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            List<Person> people = new List<Person>
+            {
+            new Person("Алиса", "женщина"),
+            new Person("Сергей", "Мужчина"),
+            new Person("Яна", "женщина"),
+            new Person("Давид", "Мужчина"),
+            new Person("Ева", "женщина"),
+            new Person("Максим", "Мужчина"),
+            new Person("Диана", "женщина"),
+            new Person("Генри", "Мужчина"),
+            };
+
+            Console.WriteLine("Выберите критерий сортировки:");
+            Console.WriteLine("1 - По имени");
+            Console.WriteLine("2 - По полу");
+            int choice = int.Parse(Console.ReadLine());
+
+            List<Person> sortedPeople;
+            switch (choice)
+            {
+                case 1:
+                    sortedPeople = people.OrderBy(p => p.Name).ToList();
+                    break;
+                case 2:
+                    sortedPeople = people.OrderBy(p => p.Gender).ToList();
+                    break;
+                default:
+                    Console.WriteLine("Неправильный выбор, сортировка не выполнена.");
+                    return;
+            }
+
+            Console.WriteLine("Отсортированный список:");
+            PrintPeopleList(sortedPeople);
+        }
+
+        public static void PrintPeopleList(List<Person> people)
+        {
+            foreach (var person in people)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
+
